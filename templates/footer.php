13fdbb05b40fdacd0e9bf0f40ef0546706334193
guinafelix <oguilhermeleitefelix@gmail.com>
2023-04-15 10:58:03 -0300

    feat: initial configuration

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer>
+  <p>bloguina-php &copy; 2023</p>
+</footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<?php
+  include_once("helpers/url.php")
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>blog-php</title>
+  <link rel="stylesheet" href="<?=  $BASE_URL ?>/css/styles.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <header>
+    template cabeçalhho
+  </header>
